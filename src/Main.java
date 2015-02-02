9f8f7367fdb61edb136f3ac8affca27bbbf519ce
Quentin <mrtchiki@gmail.com>
2015-02-02 15:17:12 +0100

    Client REST

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,10 @@ public class Main {
     public static void main(String[] args)
             throws Exception
     {
-
+        EUtilClient util = new EUtilClient();
+        util.esearchGIByLocusID("Eukaryota");
+        System.out.println(util.toString());
     }
+
+
 }
