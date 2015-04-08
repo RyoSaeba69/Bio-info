23153633a442a14b0e538d1e67450fb085438e86
Jean-François <jf67.muller@gmail.com>
2015-04-08 18:54:25 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
+import com.sun.deploy.util.StringUtils;
+import erest.BioHashMap;
 import erest.EUtilClient;
+import models.Genom;
+
+import java.util.ArrayList;
+import java.util.StringJoiner;
 
 
 public class Main {
@@ -8,10 +14,18 @@ public class Main {
     {
         EUtilClient util = new EUtilClient();
 
-        util.esearchGIByLocusID("Eukaryota");
+        BioHashMap<String, String> opts = new BioHashMap<String, String>();
+      //  util.esearchId("eukaryota", opts);
+        ArrayList<String> allIds = util.esearchAllId("eukaryota", opts);
+//        ArrayList<Genom> seqRes = util.efetchGenomsByIds(new ArrayList<String>(allIds.subList(0, 20)));
+        ArrayList<Genom> seqRes = util.efetchGenomsByIds(allIds);
 
-        System.out.println(util.toString());
+
+
+        System.out.println("SeqRes => ");
+       // String xml = util.efetchSeqById(allIds.get(0));
+      //  String idcoma = StringUtils.join(allIds, ",");
+        //System.out.println(idcoma);
+
     }
-
-
 }
